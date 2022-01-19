565a5ff3c974da8d2b46bfe47452f848a4cd874b
shahzebAM <shahzebali654321@gmail.com>
2022-01-19 12:14:31 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import arr from './upload';
+
+const MappingCard = () => {
+
+  return (
+
+    <div className='main'>
+      {arr.map((value) => (
+
+        <Card className={'mycard'} style={{ backgroundColor: '#C5D8D1', marginTop: 20}} variant='outlined' sx={{ maxWidth: '15rem', marginLeft: 'auto', marginRight: 'auto' }}>
+          <CardContent style={{backgroundColor: '#6096BA', padding: 0}}>
+            <Typography style={{color: '#000', backgroundColor: '#C5D8D1', fontWeight: 'bold'}} align='center' gutterBottom variant="h6" component="div">
+              {value.name}
+            </Typography>
+
+          </CardContent>
+          
+          <CardMedia style={{backgroundColor: '#C5D8D1'}}
+          >
+            <iframe frameborder="0" allow="autoplay" allowfullscreen="true" style={{ width: '100%', height: '100%' }} src={value.src}></iframe>
+          </CardMedia>
+          
+          <CardActions style={{ justifyContent: 'center', backgroundColor: '#C5D8D1' }}>
+            <Button variant='contained' style={{backgroundColor: '#12263A'}} size="small" href={value.link}>Download</Button>
+            
+          </CardActions>
+        </Card>
+
+      ))}
+
+    </div>
+  )
+}
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {MappingCard()}
     </div>
-  );
-}
 
+  )
+}
 export default App;
+
---
+++ src/upload.js
@@ -0,0 +1,42 @@
+const arr = [
+    {
+        name: "Episode 77",
+       src: "//ok.ru/videoembed/3233411631776",
+       link: "https://ok6-13.vkuser.net/?expires=1640953091277&srcIp=39.51.255.39&pr=10&srcAg=CHROME&ms=95.142.206.140&type=3&sig=2nQyGJiQUds&ct=0&urls=45.136.21.76%3B185.226.53.74&clientType=1&zs=43&id=1992287390237"
+   },
+       {
+      
+       name: "Episode 76",
+       src: "//ok.ru/videoembed/3231625382458",
+       link: "https://ok6-4.vkuser.net/?expires=1640345244127&srcIp=39.52.179.3&pr=10&srcAg=CHROME&ms=95.142.206.131&type=3&sig=w9nekC_r6J4&ct=0&urls=185.226.53.74%3B185.226.52.76&clientType=1&zs=43&id=1938843437597"
+   },
+   
+      {
+      
+       name: "Episode 75",
+       src: "//ok.ru/videoembed/3996665645597",
+       link: "https://ok6-14.vkuser.net/?expires=1639743717450&srcIp=203.135.47.120&pr=10&srcAg=CHROME&ms=95.142.206.141&type=3&sig=t-sv6k2i7To&ct=0&urls=45.136.22.38%3B185.226.52.58&clientType=1&zs=43&id=1892770122269"
+   },
+    
+     {
+      
+       name: "Episode 74",
+       src: "//ok.ru/videoembed/3918316702237",
+       link: "https://ok6-7.vkuser.net/?expires=1639130524164&srcIp=119.63.139.31&pr=10&srcAg=CHROME&ms=95.142.206.134&type=3&sig=hJw8oa0MjY4&ct=0&urls=45.136.22.79%3B185.226.52.34&clientType=1&zs=43&id=1866315926045"
+   },
+    
+    {
+      
+       name: "Episode 73",
+       src: "//ok.ru/videoembed/3837268331037",
+       link: "https://ok6-5.vkuser.net/?expires=1638539054210&srcIp=103.18.22.234&pr=10&srcAg=CHROME&ms=95.142.206.132&type=3&sig=ISsyOGdOXyM&ct=0&urls=45.136.21.76%3B45.136.22.72&clientType=1&zs=43&id=1846011234845"
+   },
+   
+   {
+      
+       name: "Episode 72",
+       src: "//ok.ru/videoembed/3400994720501",
+       link: "https://ok6-1.vkuser.net/?expires=1637915967272&srcIp=119.63.139.54&pr=10&srcAg=CHROME&ms=95.142.206.128&type=3&sig=UH2jKmvBdwQ&ct=0&urls=45.136.21.75%3B185.226.52.30&clientType=1&zs=43&id=1936254307061"
+   }
+]
+export default arr;
