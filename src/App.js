530bd92f5eca2bb7d2acc2c858ba9dd5dd72e0cd
shahzebAM <76158680+shahzebAM@users.noreply.github.com>
2022-01-28 06:44:03 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ const MappingCard = () => {
       {arr.map((value) => (
 
         <Card className={'mycard'} style={{ backgroundColor: '#BFD7EA', marginTop: 20}} variant='outlined' sx={{ maxWidth: '15rem', marginLeft: 'auto', marginRight: 'auto' }}>
-          <CardContent style={{backgroundColor: '#6096BA', padding: 0}}>
-            <Typography style={{color: '#204B57', backgroundColor: '#BFD7EA', fontWeight: 'bold'}} align='center' gutterBottom variant="h6" component="div">
+          <CardContent style={{backgroundColor: '#AD6A6C', padding: 0}}>
+            <Typography style={{color: '#204B57', backgroundColor: '#AD6A6C', fontWeight: 'bold'}} align='center' gutterBottom variant="h6" component="div">
               {value.name}
             </Typography>
 
@@ -27,8 +27,8 @@ const MappingCard = () => {
             <iframe frameborder="0" allow="autoplay" allowfullscreen="true" style={{ width: '100%', height: '100%' }} src={value.src}></iframe>
           </CardMedia>
           
-          <CardActions style={{ justifyContent: 'center', backgroundColor: '#BFD7EA' }}>
-            <Button variant='contained' style={{backgroundColor: '#0FA3B1'}} size="small" href={value.link}>Download</Button>
+          <CardActions style={{ justifyContent: 'center', backgroundColor: '#AD6A6C' }}>
+            <Button variant='contained' style={{backgroundColor: '#5D2E46'}} size="small" href={value.link}>Download</Button>
             
           </CardActions>
         </Card>
