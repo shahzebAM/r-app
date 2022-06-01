27e2bfc00e10a208c5a373c6c7a4ad3ae8f9c34b
shahzebAM <76158680+shahzebAM@users.noreply.github.com>
2022-06-01 17:53:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const MappingCard = () => {
 
           </CardContent>
           
-          <CardMedia style={{backgroundColor: '##f2d9d9'}}
+          <CardMedia style={{backgroundColor: '#602020'}}
           >
             <iframe frameborder="0" allow="autoplay" allowfullscreen="true" style={{ width: '100%', height: '100%' }} src={value.src}></iframe>
           </CardMedia>
