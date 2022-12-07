d395c93bb9b21dbd305856bbe609cc7ea2f3ba0f
shahzebAM <76158680+shahzebAM@users.noreply.github.com>
2022-12-08 04:25:40 +0500

    Update upload.js

--- src/upload.js
+++ src/upload.js
@@ -3,25 +3,25 @@ const arr =
 
    {
        name: "Episode 106",
-       src: "https://player.vimeo.com/video/777147665",
+       src: "https://www.aparat.com/video/video/embed/videohash/UE052/vt/frame",
        link: "https://player.vimeo.com/progressive_redirect/download/776867290/container/a5e62fd7-4267-474a-9c0e-8fc1413ce6d2/92abf50b-8196b9b4/ko106urhd.mp4%20%28720p%29.mp4?expires=1669964113&loc=external&signature=73f74152530625038ddc1a9dfafc8f991f07fab66caf9bb195c82f13b848c749"
    },
 
    {
        name: "Episode 105",
-       src: "https://player.vimeo.com/video/774651788",
+       src: "https://www.aparat.com/video/video/embed/videohash/BrdMz/vt/frame",
        link: "https://player.vimeo.com/progressive_redirect/download/774651788/container/35147caf-fbd8-4216-85b3-7eb1939d0713/779d66a0-b57e160c/ko105ur1080p%20%28720p%29.mp4?expires=1669365061&loc=external&signature=dbc3721f70b1318eef1798e5708675f28c81a3decf8d785e77f45f362d409e69",
    },
    
    {
        name: "Episode 104",
-       src: "https://player.vimeo.com/video/772255473",
-       link: "https://download-video.akamaized.net/2/download/ea412e5e-6711-449c-8d0c-d6843ee5f04d/7705bf77-0d6be069/ko104ur1080p%20%28720p%29.mp4?__token__=st=1668763316~exp=1668849802~acl=%2F2%2Fdownload%2Fea412e5e-6711-449c-8d0c-d6843ee5f04d%2F7705bf77-0d6be069%2Fko104ur1080p%2520%2528720p%2529.mp4%2A~hmac=f0b7783259fe12abfda44cb963198d726df7f36da4cd92bdac899e6746c095bb&r=dXMtY2VudHJhbDE%3D",
+       src: "https://www.aparat.com/video/video/embed/videohash/ECKq7/vt/frame",
+       link: "https://player.vimeo.com/progressive_redirect/download/774651788/container/35147caf-fbd8-4216-85b3-7eb1939d0713/779d66a0-b57e160c/ko105ur1080p%23316~exp=1668849802~acl=%2F2%2Fdownload%2Fea412e5e-6711-449c-8d0c-d6843ee5f04d%2F7705bf77-0d6be069%2Fko104ur1080p%2520%2528720p%2529.mp4%2A~hmac=f0b7783259fe12abfda44cb963198d726df7f36da4cd92bdac899e6746c095bb&r=dXMtY2VudHJhbDE%3D",
    },
    
    {
        name: "Episode 103",
-       src: "https://player.vimeo.com/video/770130235",
+       src: "https://www.aparat.com/video/video/embed/videohash/WNCfB/vt/frame",
        link: "#",
    },
    
